2c26ed9b2bc800f01660b974e0c35ed8cd557e32
broleomo <brooklynnmoor@gmail.com>
2017-08-01 17:03:10 -0400

    finished assignment

---
+++ main.js
@@ -0,0 +1,40 @@
+
+// console.log(customers);
+
+let container = document.querySelector('.container');
+
+// function createCustomers () {
+  // create for loop for amount of data set = 12 (info.results[i])
+  for (let i = 0;i < customers.results.length; i++){
+    let directory = document.createElement('div');
+    directory.innerHTML = `
+    <div class="photo">
+    <img src= ${customers.results[i].picture.large}>
+    </div>
+    <div class="name">
+    ${customers.results[i].name.first}
+    ${customers.results[i].name.last}
+    </div>
+    <div class="email">
+    ${customers.results[i].email}
+    </div>
+    <div class="address">
+    ${customers.results[i].location.street}
+    ${customers.results[i].location.city}
+    ${customers.results[i].location.state}
+    ${customers.results[i].phone}
+    </div>
+    `;
+container.appendChild(directory);
+}
+
+console.log();
+
+
+
+
+
+// document.querySelector('.container').innerHTML = template
+// console.log(createCustomers);
+
+// document.querySelector('.container').innerHTML = template
